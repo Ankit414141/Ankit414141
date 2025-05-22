4c74bf35003de16427a091a19f95df783f1a2a50
Ankit <dabhiankit38@gmail.com>
2025-05-22 12:06:31 +0530

    Add files via upload

---
+++ 41_revres_exe.c
@@ -0,0 +1,48 @@
+/* write a program to reverse given number 
+    input : 1234 output 4321
+    steps
+    ------------------------------------------
+    1)  create variable number, reminder, reverse  =0
+    2)  accept input into number(1234)
+    
+    3)  get last digit 
+        reminder(4) = number % 10 
+    4)  add reminder into reverse 
+        reverse(4) = (reverse * 10) + reminder
+    5)  number(123) = number / 10 123
+    
+    6)  reminder(3) = number % 10 
+    7)  reverse 43 = (reverse * 10) + reminder
+    8)  number(12) = number / 10 1
+    
+    9)  reminder(2) = number % 10 
+    10) reverse 432 = (reverse * 10) + reminder
+    11) number(1) = number / 10
+*/
+#include<stdio.h>
+void main()
+{
+
+    int num,reminder,reverse=0,
+    printf("enter a number:-");
+    scanf("%d",&num);
+    for(count=0;num>0;count++)
+    {
+    reminder = num % 10 ;   
+    reverse = (reverse * 10) + reminder;
+    num = num / 10 ; 
+    
+    
+
+    }
+    
+     printf("%d", reverse);
+    
+
+    
+
+
+
+
+
+}
