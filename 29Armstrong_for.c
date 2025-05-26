e5a83a3eeaf4e86841fdafc9b2bfe742fdd45dcd
Ankit <dabhiankit38@gmail.com>
2025-05-26 13:47:24 +0530

    Add files via upload

--- 29Armstrong_for.c
+++ 29Armstrong_for.c
@@ -1,12 +1,39 @@
-// write a program to findout whether given number is Armstrong   Numbers or not 
-/*1 creat variablde num,
+/* armstrong number  and not
+1,-creat a variabal=n,arm,r,c
+2- input in accept user num =153
+3- steps:__
+   3.1  r=n%10=(3)
+     arm=(r*r*r)+arm(27+0)
+     n= n/10(15\10=1)
+     if stetmant of print 
+     if (c==n)
+     dispaly in result
+
+
 */
 #include<stdio.h>
 void main()
 {
-    int num,multi;
-    printf("enter the digite");
-    scanf("%d",&num);
-    printf("%d*%d*%d")
+    int n,arm=0,r,c;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    c=n;
+    while (n>0)
+    {
+        r=n%10;
+        arm=(r*r*r)+arm;
+        n=n/10;
 
+    }
+    if(c==arm)
+    {
+        printf("this a armstrong number");
+
+    }
+    else{
+        printf("this not a armstrong number");
+
+        
+    }
+    
 }
