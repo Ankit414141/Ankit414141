76cb1002d29930f11aabc200a63181d0af2c8ac3
Ankit <dabhiankit38@gmail.com>
2025-05-31 00:08:31 +0530

    Add files via upload

---
+++ 56_worad_string_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main() {
+    char str[1000];
+    int i = 0, words = 1;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);  // Read input including spaces
+
+    while (str[i] != '\0') {
+        // Count space characters to estimate words
+        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n') {
+            words++;
+        }
+        i++;
+    }
+
+    printf("Total number of words: %d\n", words);
+
+
+}
